23b05a4df3dd2cb59e715959fb94cc7438bb30cd
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-05-26 12:44:45 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import numpy as np
+from flask import Flask, jsonify
+
+## __name__ é definido por padrão. Poderiamos usar outra string qualquer aqui, porém,
+## não recomendo, pois, isso poderia causar problemas em aplicações maiores.
+app = Flask(__name__)
+
+# definimos aqui uma rota, no caso criamos a rota localhost:5000/
+@app.route("/")
+def primeiro_endpoint_get():
+  return ("Tudo Funcionando Corretamente !", 200) 
+
+@app.route("/predict")
+def segundo_endpoint():
+  return ("Chamando o predict", 200)
+
+if __name__ == "__main__":
+  debug = True # com essa opção como True, ao salvar, o "site" recarrega automaticamente.
+  app.run(host='0.0.0.0', port=5000, debug=debug)
