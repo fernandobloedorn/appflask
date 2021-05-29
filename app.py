11f7ac445677f61beec28134d97506e6ffaf11c6
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-05-29 07:31:40 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import numpy as np
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 ## __name__ é definido por padrão. Poderiamos usar outra string qualquer aqui, porém,
 ## não recomendo, pois, isso poderia causar problemas em aplicações maiores.
@@ -10,9 +10,14 @@ app = Flask(__name__)
 def primeiro_endpoint_get():
   return ("Tudo Funcionando Corretamente !", 200) 
 
-@app.route("/predict")
+@app.route("/predict", methods=["POST"])
 def segundo_endpoint():
-  return ('{ "status": "Gera receita", "accuracy": 87.23 }', 200)
+
+  body = request.get_json()
+
+  print(body)
+
+  return (body, 200)
 
 if __name__ == "__main__":
   debug = True # com essa opção como True, ao salvar, o "site" recarrega automaticamente.
