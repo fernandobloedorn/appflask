38763c6d43ea30c2ad592b2ec23ebbd6e9a51ab4
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-06-07 13:16:32 -0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import pickle
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_predict
@@ -7,3 +8,10 @@ from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, r
 model = pickle.load(open('model_rcf', 'rb'))
 
 print(model)
+
+new = np.array([0.0, 0.0, 0.0, 0.0, 2.0, 2.666667, 0.050000, 0.140000, 0.0, 0.0, 2.0, 3.0, 2.0, 2.0, 4.0, 2.0, 0.0]).reshape( 1, -1)
+pred = model.predict(new)
+print(pred)
+
+pred_proba = model.predict_proba(new)
+pred_proba
