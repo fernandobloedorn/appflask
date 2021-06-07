2b993d62ede0959abce13ec72a2f50900a4966a5
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-06-07 13:03:01 -0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score, classification_report, mean_absolute_error, mean_squared_error
 
 model = pickle.load(open('model_rcf', 'rb'))
 
