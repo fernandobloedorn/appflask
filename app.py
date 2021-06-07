96d21c805caf85f6b2557fc1c91bedf674d76633
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-06-07 14:28:28 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -45,28 +45,28 @@ def segundo_endpoint():
 
   # print("Exam: 0.0, 0.0, 0.0, 0.0, 2.0, 2.666667, 0.050000, 0.140000, 0.0, 0.0, 2.0, 3.0, 2.0, 2.0, 4.0, 2.0, 0.0")
 
-  model = pickle.load(open('model_rcf', 'rb'))
+  # model = pickle.load(open('model_rcf', 'rb'))
 
-  new = np.array([0.0, 0.0, 0.0, 0.0, 2.0, 2.666667, 0.050000, 0.140000, 0.0, 0.0, 2.0, 3.0, 2.0, 2.0, 4.0, 2.0, 0.0]).reshape( 1, -1)
-  pred = model.predict(new)
-  pred_proba = model.predict_proba(new)
+  # new = np.array([0.0, 0.0, 0.0, 0.0, 2.0, 2.666667, 0.050000, 0.140000, 0.0, 0.0, 2.0, 3.0, 2.0, 2.0, 4.0, 2.0, 0.0]).reshape( 1, -1)
+  # pred = model.predict(new)
+  # pred_proba = model.predict_proba(new)
 
-  json = '{"revenue":' + ('True' if pred[0] else 'False') + ', "correct": '
+  # json = '{"revenue":' + ('True' if pred[0] else 'False') + ', "correct": '
 
-  correct = 0.0
-  incorrect = 0.0
+  # correct = 0.0
+  # incorrect = 0.0
 
-  if len(pred_proba) > 0:
-    l = len(pred_proba[0])
-    if l > 0:
-      correct = pred_proba[0][0]*100
-      if l > 1:
-        incorrect = pred_proba[0][1]*100
+  # if len(pred_proba) > 0:
+  #   l = len(pred_proba[0])
+  #   if l > 0:
+  #     correct = pred_proba[0][0]*100
+  #     if l > 1:
+  #       incorrect = pred_proba[0][1]*100
 
-  json += str(correct) + ', "incorrect": ' + str(incorrect) + '}'
+  # json += str(correct) + ', "incorrect": ' + str(incorrect) + '}'
 
-  # return ({ "message": "Nao gera receita com 99,40% de acuracidade."}, 200) 
-  return (json, 200) 
+  return ({ "message": "Nao gera receita com 99,40% de acuracidade."}, 200) 
+  # return (json, 200) 
 
 if __name__ == "__main__":
   debug = True # com essa opção como True, ao salvar, o "site" recarrega automaticamente.
